bb52c4dc75cc25ff90929c2f70f787e35b64f8ff
Charlie Wilson <charlie.wilson4@gmail.com>
2022-12-02 14:23:15 +1100

    Add configuration from settings

---
+++ ninja_decorators/conf.py
@@ -0,0 +1,23 @@
+from django.conf import settings as django_settings
+from pydantic import BaseModel, Field
+
+
+class Settings(BaseModel):
+    """Alter these by modifying the values in Django's settings module (usually `settings.py`)."""
+
+    # FILTER_CLASS: str = Field(
+    #     "ninja_decorators.filtering.ORMFilter", alias="NINJA_DECORATORS_FILTER_CLASS"
+    # )
+    # SORTING_CLASS: str = Field(
+    #     "ninja_decorators.sorting.ORMSorting", alias="NINJA_DECORATORS_SORTING_CLASS"
+    # )
+    PERMISSIONS_CLASS: str = Field(
+        "ninja_decorators.permissions.DjangoAuthPermissions",
+        alias="NINJA_DECORATORS_PERMISSIONS_CLASS",
+    )
+
+    class Config:
+        orm_mode = True
+
+
+settings = Settings.from_orm(django_settings)
